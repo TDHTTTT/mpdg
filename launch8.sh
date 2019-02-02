74f2e07fed623f07b2a7eb2340c9b0b466032eeb
TDHTTTT <tdhttt@hotmail.com>
2019-02-02 11:48:49 -0800

    modify to run in parallel

--- launch8.sh
+++ launch8.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-# ./launch.sh #runs #events/run
+# ./launch.sh #runs #events/run #randseed_start
 
+mkdir Run_${3}
+cp *.tar.gz Run_${3}
+cd Run_${3}
 tar -xzf *.tar.gz
+mv expgrid/* ./
 chmod -R 700 ./
-cd madevent/bin
+cd ./madevent/bin
 ./compile
-cd ../..
+cd ../../
 
-mkdir Events Roots
-for ((i=1; i<=$1; i++));
+#mkdir Events Roots
+
+for ((i=$3; i<$1+$3; i++));
 do
-    ./run.sh $2 $i
-    mv events.lhe.gz ./Events/events_${i}.lhe.gz
+    ../run.sh $2 $i
+    mv events.lhe.gz ../Events/events_${i}.lhe.gz
 done
 
 mkdir tmp
-cp ./process8_lhef.sh ./tmp
+cp ../process8_lhef.sh ./tmp
 cd tmp
-./process8_lhef.sh $1
+./process8_lhef.sh $1 $3
 
 cd ../
-mv ./tmp/*.root ./Roots
-rm -rf tmp
+mv ./tmp/*.root ../Roots
+#rm -rf tmp
--- process8_lhef.sh
+++ process8_lhef.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-DELPHES_DIR=/mnt/c/Users/tdhttt/workspace/hep/delphes/Delphes-3.4.1
+# ./process8_lhef.sh #runs #randseed_start
 
-for ((i=1; i<=$1; i++));
+DELPHES_DIR=/pub/daohangt/Delphes-3.4.1
+
+for ((i=$2; i<$1+$2; i++));
 do
-    if [ -e "../Roots/delphes_${i}.root" ]; then
+    if [ -e "../../Roots/delphes_${i}.root" ]; then
         echo "Delphes file already exists! Abort."
         exit 1
     fi
 
     # unzip the input events, if necessary
-    if [ ! -e "../Events/events_${i}.lhe" ]; then
-        if [ -e "../Events/events_${i}.lhe.gz" ]; then
-            cp ../Events/events_${i}.lhe.gz ./unweighted_events.lhe.gz
+    if [ ! -e "../../Events/events_${i}.lhe" ]; then
+        if [ -e "../../Events/events_${i}.lhe.gz" ]; then
+            cp ../../Events/events_${i}.lhe.gz ./unweighted_events.lhe.gz
             gunzip -f ./unweighted_events.lhe.gz
             if [ $? -ne 0 ]; then
                 echo "Failed to unzip LHE!" >&2
@@ -24,7 +26,7 @@ do
         fi
     fi
 
-    $DELPHES_DIR/DelphesPythia8 ../Cards/delphes_card.tcl ../Cards/configLHE.cmnd delphes_${i}.root
+    $DELPHES_DIR/DelphesPythia8 ../../Cards/delphes_card.tcl ../../Cards/configLHE.cmnd delphes_${i}.root
     delphes_code=$?
     if [ $delphes_code -ne 0 ]; then
         echo "Delphes returned error! Code=$delphes_code" >&2
