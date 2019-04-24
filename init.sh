fb703f5f47da281f45e214a70a96f391db6a5bf3
TDHTTT <tdhttt@hotmail.com>
2019-04-23 20:55:50 -0700

    new dir new init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 # source init.sh
 
-export DELPHES=/c/Users/tdhttt/workspace/hep/delphes/Delphes-3.4.1
-export export LD_LIBRARY_PATH=$DELPHES:$LD_LIBRARY_PATH
+export DELPHES=/pub/daohangt/hep/Delphes-3.4.1
+export LD_LIBRARY_PATH=$DELPHES:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=./:$DELPHES:$DELPHES/external:$ROOT_INCLUDE_PATH
