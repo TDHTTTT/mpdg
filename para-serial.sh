05a2dc26b3e5b2ffc650fa465f34058880c8a980
TDHTTT <tdhttt@hotmail.com>
2019-05-15 01:50:11 -0700

    more conservative core usage

--- para-serial.sh
+++ para-serial.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#$ -N ppjj_225a
+#$ -N <NAME-OF-THE-JOB>
 #$ -ckpt restart
 #$ -q free*
-#$ -pe openmp 8
+#$ -pe openmp 4
 
 # Try to be conservative before figuring out low cpu
 ./launch8.sh $1 $2 $3 > log_${3}
